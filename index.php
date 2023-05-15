6b22132db0d23a572c134ff710104803ac1ca2f8
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2023-05-15 19:55:00 -0300

    Update files

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP - Sistemas WEB</title>
+</head>
 <body>
+
+
 
 <?php
 echo "meu primeiro script em PHP!";
 ?>
 
+
+
+
+
+
+    
+
+<scrript src="script.js" defer></scrript>
 </body>
 </html>
+
+
